f9e7f5231df9a948734e4dad692be5a37c04b367
florian <florian.carriere@gmail.com>
2021-12-23 22:02:10 +0100

    switch to mysql (+ update timezone)

--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '/home/dev/app_django/polls/poll_db',
+        'USER': 'florian',
+        'PASSWORD': 'boubou'
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'FR'
 
 USE_I18N = True
 
